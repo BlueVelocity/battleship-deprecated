2dd35fc2f0886912fc4ed22ec4ce80ef76979c69
BlueVelocity <josephrodney@hotmail.com>
2024-05-24 23:48:45 -0400

    Refactor ship object and tests

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,3 +1,27 @@
-const createShip = function (shipLength, orientation = 1) {};
+const createShip = function (length) {
+  if (typeof length !== "number")
+    throw new Error("InputError: Expected number for length");
+
+  const validLengthRange = [1, 5];
+  if (length < validLengthRange[0] || length > validLengthRange[1])
+    throw new Error("InputError: Outside valid range");
+
+  let hits = 0;
+
+  function hit() {
+    hits++;
+  }
+
+  function isSunk() {
+    if (hits === length) return true;
+    return false;
+  }
+
+  return {
+    length,
+    hit,
+    isSunk,
+  };
+};
 
 export { createShip };
