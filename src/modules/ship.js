0fa13f691165d8a8b14f600970ba33206a5e8362
BlueVelocity <josephrodney@hotmail.com>
2024-05-24 23:15:29 -0400

    Remove ship module implementation

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,23 +1,3 @@
-const createShip = function (shipLength, orientation = 1) {
-  // Orientation 1:up, 2:right, 3:down, 4:left
-  if (orientation < 1 || orientation > 4 || typeof orientation !== "number") {
-    throw new Error("InputError: Invalid orientation");
-  }
-
-  switch (shipLength) {
-    case 5:
-      return { shipType: "carrier", length: 5, orientation };
-    case 4:
-      return { shipType: "battleship", length: 4, orientation };
-    case 3:
-      return { shipType: "cruiser", length: 3, orientation };
-    case 2:
-      return { shipType: "submarine", length: 2, orientation };
-    case 1:
-      return { shipType: "destroyer", length: 1, orientation };
-    default:
-      throw new Error("InputError: Invalid ship length");
-  }
-};
+const createShip = function (shipLength, orientation = 1) {};
 
 export { createShip };
