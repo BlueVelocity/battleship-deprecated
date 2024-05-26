7cbbcbfce21c9905a9825007014cdcbf6ca205e5
BlueVelocity <josephrodney@hotmail.com>
2024-05-26 18:29:15 -0400

    Add gameboard construction and ship placement with tests
    Ship placement incomplete

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,1 +1,57 @@
+import { createShip } from "../modules/ship.js";
+
+const tile = function () {
+  return {
+    occupant: null,
+  };
+};
+
+const createBoard = function (boardSize = 10) {
+  const board = (function () {
+    const tempBoard = [];
+    for (let x = 0; x < boardSize; x++) {
+      const column = [];
+      for (let y = 0; y < boardSize; y++) {
+        column.push(tile());
+      }
+      tempBoard.push(column);
+    }
+    return tempBoard;
+  })();
+
+  const place = function (shipLength, coordinates, orientation = 1) {
+    if (!Array.isArray(coordinates))
+      throw new Error("InputError: Invalid coordinate data type");
+
+    if (
+      typeof coordinates[0] !== "number" ||
+      typeof coordinates[1] !== "number"
+    )
+      throw new Error("InputError: Coordinates contain invalid data type");
+
+    if (
+      coordinates[0] < 0 ||
+      coordinates[0] > boardSize ||
+      coordinates[1] < 0 ||
+      coordinates[1] > boardSize
+    )
+      throw new Error("InputError: Coordinates outside range");
+
+    const ship = createShip(shipLength);
+
+    for (let i = 0; i < ship.length; i++) {
+      board[Math.round(coordinates[0])][
+        Math.round(coordinates[1] + i)
+      ].occupant = ship;
+    }
+  };
+
+  return {
+    get board() {
+      return board;
+    },
+    place,
+  };
+};
+
 export { createBoard };
--- tests/gameBoard.spec.js
+++ tests/gameBoard.spec.js
@@ -1,1 +1,75 @@
 import { createBoard } from "../src/modules/gameBoard.js";
+
+test("returns board with grid", () => {
+  expect(createBoard().board[0][0]).toBeDefined();
+});
+
+test("place() places ship at specified coordinates", () => {
+  const testCases = [2, 3, 4, 5];
+  const originCoords = [5, 5];
+
+  testCases.forEach((length) => {
+    const testBoard = createBoard(10);
+    testBoard.place(length, originCoords);
+
+    for (let i = 0; i < length; i++) {
+      expect(
+        testBoard.board[originCoords[0]][originCoords[1] + i].occupant,
+      ).toBeTruthy();
+    }
+  });
+});
+
+test("place() throws error if passed invalid coordinate data type", () => {
+  const testCases = [undefined, null, "string", {}];
+
+  testCases.forEach((invalidCoords) => {
+    expect(() => createBoard().place(1, invalidCoords)).toThrow(
+      "InputError: Invalid coordinate data type",
+    );
+  });
+});
+
+test("place() throws error if coordinates are outside of range", () => {
+  const boardSize = 10;
+  const testCases = [
+    [-1, -1],
+    [-1, 0],
+    [11, 11],
+    [0, 11],
+  ];
+
+  testCases.forEach((coordinates) => {
+    expect(() => createBoard(boardSize).place(1, coordinates)).toThrow(
+      "InputError: Coordinates outside range",
+    );
+  });
+});
+
+test("place() throws error if coordinate data type is not a number", () => {
+  const testCases = [
+    ["", 0],
+    [{}, 0],
+    ["", []],
+  ];
+
+  testCases.forEach((invalidCoords) => {
+    expect(() => createBoard().place(1, invalidCoords)).toThrow(
+      "InputError: Coordinates contain invalid data type",
+    );
+  });
+});
+
+test("place() accepts decimal", () => {
+  const testCases = [
+    [1.5, 2],
+    [1.4, 1],
+    [1.6, 2],
+  ];
+
+  testCases.forEach((coordinates) => {
+    const testBoard = createBoard();
+    testBoard.place(1, [coordinates[0], 0]);
+    expect(testBoard.board[coordinates[1]][0].occupant).toBeTruthy();
+  });
+});
