8b5522c51380fb21ee26984e8139195b0e1db0ba
BlueVelocity <josephrodney@hotmail.com>
2024-05-30 22:18:27 -0400

    Allows a player to be designated a computer

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,14 +1,25 @@
 import { createBoard } from "./gameBoard.js";
 
-const createPlayer = function (name) {
+const createPlayer = function (name, isComputer = false) {
   if (typeof name === "undefined")
     throw new Error("InputError: Undefined name");
 
   if (typeof name !== "string") throw new Error("InputError: Invalid name");
 
+  if (
+    isComputer !== true &&
+    isComputer !== 1 &&
+    isComputer !== false &&
+    isComputer !== 0
+  )
+    throw new Error("InputError: Expected boolean for player type selection");
+
+  if (isComputer === 1) isComputer = true;
+  if (isComputer === 0) isComputer = false;
+
   const gameBoard = createBoard();
 
-  return { name, gameBoard };
+  return { name, isComputer, gameBoard };
 };
 
 export { createPlayer };
--- tests/player.spec.js
+++ tests/player.spec.js
@@ -26,3 +26,20 @@ test("throws error if name is not a string", () => {
     expect(() => createPlayer(input)).toThrow("InputError: Invalid name");
   });
 });
+
+test("creates computer or player based on type", () => {
+  expect(createPlayer("testName", true).isComputer).toBe(true);
+  expect(createPlayer("testName", 1).isComputer).toBe(true);
+  expect(createPlayer("testName", false).isComputer).toBe(false);
+  expect(createPlayer("testName", 0).isComputer).toBe(false);
+});
+
+test("throws error if player type selection is not true, 1, false, or 0", () => {
+  const invalidInputs = [3, [], {}, "true"];
+
+  invalidInputs.forEach((input) => {
+    expect(() => {
+      createPlayer("testName", input);
+    }).toThrow("InputError: Expected boolean for player type selection");
+  });
+});
