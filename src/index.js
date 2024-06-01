ae914cc7509dcda148618cb24c3166f50ee8a5a5
BlueVelocity <josephrodney@hotmail.com>
2024-05-31 21:11:23 -0400

    Integrate tailwind and update stylesheet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./style.css";
-
-const welcome = document.createElement("p");
-welcome.textContent = "If this text is red, then template is working!";
+import { createTitleblock } from "./components/titleBlock.js";
 
 const body = document.querySelector("body");
+const titleBlock = createTitleblock("BATTLESHIP");
+const welcome = document.createElement("p");
+
+body.appendChild(createTitleblock("BATTLESHIP").component);
+
+welcome.textContent = "If this text is red, then template is working!";
 body.appendChild(welcome);
