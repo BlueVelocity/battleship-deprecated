f0c81ccaaffa0772324e8d583aa2b45abedc0121
BlueVelocity <josephrodney@hotmail.com>
2024-06-05 18:01:39 -0400

    Create game area and show name inputs and start button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import "./style.css";
 import { createHeader } from "./components/titleBlock.js";
-import { createPlayArea } from "./components/playArea.js";
+import { createGrids } from "./components/grids.js";
 
 const body = document.querySelector("body");
 
-const titleBlock = createHeader("BATTLESHIP");
-body.appendChild(titleBlock.component);
-
-const grid = createPlayArea(10);
-body.appendChild(grid.component);
+//const playerBoards = createGrids(10);
+//body.appendChild(grid.component);
 
 const temp = document.createElement("p");
-temp.textContent = "Working";
+temp.textContent = "Placed using javascript";
 body.appendChild(temp);
