72ec8c1057131a46ee1a23812ced701bbbd84488
BlueVelocity <josephrodney@hotmail.com>
2024-06-10 14:19:43 -0400

    Complete board module with tests

--- src/modules/board.js
+++ src/modules/board.js
@@ -1,10 +1,80 @@
-const tile = () => {
-  const occupant = null;
-  return { occupant };
+import { createShip } from "./ship.js";
+
+const generateGrid = () => {
+  const grid = [];
+  for (let i = 0; i < 10; i++) {
+    const row = [];
+    for (let i = 0; i < 10; i++) {
+      row.push(null);
+    }
+    grid.push(row);
+  }
+  return grid;
 };
 
 const createBoard = () => {
-  const placeShip = (coordinates) => {};
+  const model = generateGrid();
+  const ships = [];
+
+  const place = (length, [x, y] = [], orientation = 1) => {
+    const ship = createShip(length);
+
+    if (orientation === 1) {
+      const yCoords = [];
+      for (let i = 0; i < length; i++) {
+        if (y + i > 9) return false;
+        yCoords.push(y + i);
+      }
+
+      ships.push(ship);
+      yCoords.forEach((y) => (model[x][y] = ship));
+    } else if (orientation === 2) {
+      const xCoords = [];
+
+      for (let i = 0; i < length; i++) {
+        if (x + i > 9) return false;
+        xCoords.push(x + i);
+      }
+
+      ships.push(ship);
+      xCoords.forEach((x) => (model[x][x] = ship));
+    } else if (orientation === 3) {
+      const yCoords = [];
+
+      for (let i = 0; i < length; i++) {
+        if (y - i < 0) return false;
+        yCoords.push(y - i);
+      }
+
+      ships.push(ship);
+      yCoords.forEach((y) => (model[x][y] = ship));
+    } else if (orientation === 4) {
+      const xCoords = [];
+
+      for (let i = 0; i < length; i++) {
+        if (x - i < 0) return false;
+        xCoords.push(x - i);
+      }
+
+      ships.push(ship);
+      xCoords.forEach((x) => (model[x][x] = ship));
+    }
+
+    return true;
+  };
+
+  const receiveAttack = ([x, y] = coordinate) => {
+    const targetTile = model[x][y];
+    if (targetTile !== null) {
+      targetTile.hit();
+    }
+  };
+
+  const allSunk = () => {
+    return ships.every((ship) => ship.isSunk());
+  };
+
+  return { model, place, receiveAttack, allSunk };
 };
 
 export { createBoard };
--- tests/board.spec.js
+++ tests/board.spec.js
@@ -6,7 +6,60 @@ beforeEach(() => {
   board = createBoard();
 });
 
-it("returns a 10x10 boardModel", () => {
-  expect(board.boardModel.length).toBe(10);
-  expect(board.boardModel[0].length).toBe(10);
+it("returns a 10x10 model", () => {
+  expect(board.model.length).toBe(10);
+  expect(board.model[0].length).toBe(10);
+});
+
+it("can place ships using place(). Returns true if placed", () => {
+  const shipLength = 1;
+  const targetCoordinate = [1, 1];
+
+  expect(board.model[1][1]).toBeFalsy();
+  expect(board.place(shipLength, targetCoordinate)).toBe(true);
+  expect(board.model[1][1]).toBeTruthy();
+});
+
+it("returns false if ship placement is out of bounds", () => {
+  const shipLength = 1;
+  const targetCoordinate = [100, 100];
+
+  expect(board.place(shipLength, targetCoordinate)).toBe(false);
+});
+
+it("places ship length on board from origin", () => {
+  const shipLength = 3;
+  const targetCoordinate = [5, 5];
+
+  board.place(shipLength, targetCoordinate);
+  expect(board.model[5][5]).toBeTruthy();
+  expect(board.model[5][6]).toBeTruthy();
+  expect(board.model[5][7]).toBeTruthy();
+  expect(board.model[5][8]).toBeFalsy();
+});
+
+it("returns false if ship extends out of bounds from placement", () => {
+  const shipLength = 10;
+  const targetCoordinate = [7, 7];
+
+  expect(board.place(shipLength, targetCoordinate)).toBe(false);
+  expect(board.place(shipLength, targetCoordinate, 2)).toBe(false);
+  expect(board.place(shipLength, targetCoordinate, 3)).toBe(false);
+  expect(board.place(shipLength, targetCoordinate, 4)).toBe(false);
+});
+
+it("hits a ship if it receives an attack on an occupied tile", () => {
+  const targetCoordinate = [1, 1];
+  board.place(1, targetCoordinate);
+  board.receiveAttack(targetCoordinate);
+  expect(board.model[1][1].isSunk()).toBe(true);
+});
+
+it("can report if all ships are sunk or not", () => {
+  const targetCoordinate = [1, 1];
+  board.place(1, targetCoordinate);
+
+  expect(board.allSunk()).toBe(false);
+  board.receiveAttack(targetCoordinate);
+  expect(board.allSunk()).toBe(true);
 });
