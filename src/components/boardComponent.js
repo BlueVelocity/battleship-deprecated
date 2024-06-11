11a3698b0e302045950e1706191d13a093b1ee11
BlueVelocity <josephrodney@hotmail.com>
2024-06-10 23:47:08 -0400

    Add update method and fix styling

--- src/components/boardComponent.js
+++ src/components/boardComponent.js
@@ -1,12 +1,15 @@
 const styles = {
   board: "flex flex-col aspect-square bg-blue-500",
   row: "flex flex-auto",
-  tile: "flex-auto border border-black bg-blue-500 hover:cursor-pointer transition duration-250 linear active:scale-110",
+  tileEmpty:
+    "flex-auto border border-black bg-blue-500 hover:cursor-pointer transition duration-250 linear active:scale-110",
+  tileShip: "flex-auto border border-black bg-gray-500",
+  tileHit: "flex-auto border border-black bg-red-500",
 };
 
 const generateTile = ([x, y] = []) => {
   const tile = document.createElement("div");
-  tile.classList = styles.tile;
+  tile.classList = styles.tileEmpty;
 
   tile.id = x.toString() + "," + y.toString();
 
@@ -29,11 +32,12 @@ const createBoard = (id) => {
   const height = 10;
 
   const board = document.createElement("div");
+  board.classList = styles.board;
   board.id = id;
 
   for (let y = 0; y < height; y++) {
     const row = generateRow(height, y);
-    board.appendChild(row);
+    board.insertBefore(row, board.firstChild);
   }
 
   return board;
@@ -42,7 +46,22 @@ const createBoard = (id) => {
 const createBoardComponent = (id) => {
   const component = createBoard(id);
 
-  return { component };
+  const update = (grid) => {
+    const rowList = [];
+    component.childNodes.forEach((elem) => rowList.unshift(elem));
+
+    rowList.forEach((row, y) => {
+      row.childNodes.forEach((tile, x) => {
+        if (grid[x][y] === null) {
+          tile.classList = styles.tileEmpty;
+        } else if (!grid[x][y].isSunk()) {
+          tile.classList = styles.tileShip;
+        }
+      });
+    });
+  };
+
+  return { component, update };
 };
 
 export { createBoardComponent };
